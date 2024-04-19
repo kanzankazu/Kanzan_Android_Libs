de7957368583f24c1ae5d553550f6122707afe24
Faisal Bahri <kanzankazu46@gmail.com>
2024-04-19 14:45:37 +0700

    Initial commit 3

--- kanzankazuSample/src/main/java/com/kanzankazu/sample/MyApplication.kt
+++ kanzankazuSample/src/main/java/com/kanzankazu/sample/MyApplication.kt
@@ -2,17 +2,4 @@ package com.kanzankazu.sample
 
 import androidx.multidex.MultiDexApplication
 
-class MyApplication : MultiDexApplication() {
-    companion object {
-        lateinit var instance: MyApplication
-
-        fun getApp(): MyApplication {
-            return instance
-        }
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-    }
-}
+class MyApplication : MultiDexApplication()
